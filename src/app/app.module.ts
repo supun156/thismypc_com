e752a6ef4aa0d97e3d6c8b9abe61d5a3c29659d7
supun156 <supun@smallaxe.tech>
2019-07-25 12:49:03 +0530

    Remove System files from web site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,43 +5,18 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './components/home/home.component';
 import {HttpClientModule} from '@angular/common/http';
-import {SystemComponent} from './components/system/system.component';
 import {RouterModule, Routes} from '@angular/router';
-import {AccountComponent} from './components/account/account.component';
-import {DashboardComponent} from './components/admin/dashboard/dashboard.component';
-import {LoginComponent} from './components/admin/login/login.component';
-import {UserComponent} from './components/admin/user/user.component';
-import {PcComponent} from './components/admin/pc/pc.component';
-import {SoftwareComponent} from './components/admin/software/software.component';
-import {FrontEndComponent} from './components/admin/front-end/front-end.component';
 import {AuthGuard} from './guards/auth-guard.service';
 
 // todo  need to create  rout  guard  for  login
 const appRoutes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'login', component: HomeComponent},
-    {path: 'system', component: SystemComponent},
-    {path: 'account', component: AccountComponent},
-    {
-        path: 'admin', component: DashboardComponent
-    }, {
-        path: 'admin/software', component: SoftwareComponent , canActivate: [AuthGuard],
-    }
+    {path: '', component: HomeComponent}
 ];
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent,
-        SystemComponent,
-
-        AccountComponent,
-        DashboardComponent,
-        LoginComponent,
-        UserComponent,
-        PcComponent,
-        SoftwareComponent,
-        FrontEndComponent
+        HomeComponent
     ],
     imports: [RouterModule.forRoot(
         appRoutes,
